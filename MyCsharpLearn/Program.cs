0e95d20e5f7c698a5b4499405d71cfb27e2d24b7
Saravana Kumar <saravanakumar6696@gmail.com>
2020-06-14 00:26:18 +0530

    GitIgnore File

---
+++ MyCsharpLearn/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyCsharpLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Time is " + DateTime.Now);
+        }
+    }
+}
